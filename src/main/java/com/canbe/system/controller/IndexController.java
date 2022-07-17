f61d08ad72e95c126884e82950be7f995e9c3101
canbe <824737880@qq.com>
2022-07-17 15:35:59 +0800

    加入页面

--- src/main/java/com/canbe/system/controller/IndexController.java
+++ src/main/java/com/canbe/system/controller/IndexController.java
@@ -4,8 +4,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,13 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2022-07-15 07:58
  */
 @Api(tags = "首页模块")
-@RestController
+@Controller
 public class IndexController {
 
     @ApiImplicitParam(name = "name",value = "姓名",required = true)
     @ApiOperation(value = "向客人问好")
     @GetMapping("/sayHi")
+    @ResponseBody
     public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
         return ResponseEntity.ok("Hi:"+name);
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "news-grid-3";
     }
 }
